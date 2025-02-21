bc54c2263f99aa615a9877385598f024aebc459f
Jan Henk Hazelaar <janhenk@concept7.nl>
2025-02-21 21:40:00 +0100

    Removed unused files

--- src/StatamicOptimizeStacheServiceProvider.php
+++ src/StatamicOptimizeStacheServiceProvider.php
@@ -10,16 +10,7 @@ class StatamicOptimizeStacheServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('statamic-optimize-stache')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_statamic_optimize_stache_table')
-            ->hasCommand(StatamicOptimizeStacheCommand::class);
+            ->name('statamic-optimize-stache');
     }
 }
